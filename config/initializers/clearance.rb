713552d2d392ccd89aac7f848e1f53cd59ed3ff0
Eliza Plowden <elizaplowden@gmail.com>
2021-03-15 19:26:33 +0000

    add username to model

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,4 +1,6 @@
 Clearance.configure do |config|
+  # will prevent Clearance from creating its own routes
+  config.routes = false
   config.mailer_sender = "reply@example.com"
   config.rotate_csrf_on_sign_in = true
 end
